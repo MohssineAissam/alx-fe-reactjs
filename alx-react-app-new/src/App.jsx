d3b326f11c7902c2c49278de2e19d265a896d5a5
Aissam <koala3901@gmail.com>
2025-07-21 00:09:34 +0100

    22

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,12 +18,14 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
-      <Footer />
       <UserProfile
         name="Alice"
         age="25"
         bio="Loves hiking and photography"
       />
+      <Counter /> {/* ✅ Added here */}
+
+      <Footer />
 
       <div>
         <a href="https://vite.dev" target="_blank">
